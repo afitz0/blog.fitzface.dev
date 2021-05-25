1a5d804fe22345b1e00d948a724845b2b2371410
Fitz <afitzgibbon@google.com>
2021-05-25 15:37:53 -0700

    minor refactoring/formatting

--- build.dart
+++ build.dart
@@ -4,31 +4,53 @@ import 'package:jael/jael.dart' as jael;
 import 'package:symbol_table/symbol_table.dart';
 import 'package:markdown/markdown.dart';
 import 'package:front_matter/front_matter.dart' as frontmatter;
+import 'package:path/path.dart';
+
+const POST_SRC_ROOT = "./src/";
+const TEMPLATE_ROOT = "./templates/";
+const POST_TEMPLATE = "post.html";
+const OUT_DIR = "./out";
 
 main() async {
-  String template = await File('./templates/post.html').readAsString();
-  String postMarkdown = await File('./src/post1.md').readAsString();
+  Directory postsDir = Directory(POST_SRC_ROOT);
+  final String template =
+      await File(TEMPLATE_ROOT + POST_TEMPLATE).readAsString();
 
-  var fm = frontmatter.parse(postMarkdown);
-  String htmlContent = markdownToHtml(fm.content);
+  for (var thing in postsDir.listSync()) {
+    if (thing is File) {
+      final String filenameNoExt = basenameWithoutExtension(thing.path);
+      final String fileExtension = extension(thing.path);
 
-  var document = jael.parseDocument(
-    template,
-    sourceUrl: 'stdin',
-    onError: stderr.writeln,
-  );
+      if (fileExtension != 'md') continue;
 
-  if (document == null) {
-    stderr.writeln('Could not parse the given text.');
-  } else {
-    CodeBuffer output = CodeBuffer();
-    const jael.Renderer().render(
-      document,
-      output,
-      SymbolTable(values: {'title': fm.data['title'], 'content': htmlContent}),
-      strictResolution: false,
-    );
+      final String postMarkdown = thing.readAsStringSync();
 
-    await File('./out/post1.html').writeAsString(output.toString());
+      var fm = frontmatter.parse(postMarkdown);
+      String htmlContent = markdownToHtml(fm.content);
+
+      var document = jael.parseDocument(
+        template,
+        sourceUrl: null,
+        onError: stderr.writeln,
+      );
+
+      if (document == null) {
+        stderr.writeln('Could not parse the given text.');
+      } else {
+        CodeBuffer output = CodeBuffer();
+        const jael.Renderer().render(
+          document,
+          output,
+          SymbolTable(
+              values: {'title': fm.data['title'], 'content': htmlContent}),
+          strictResolution: false,
+        );
+
+        await File(OUT_DIR + Platform.pathSeparator + filenameNoExt + '.html')
+          .writeAsString(output.toString());
+      }
+    }
   }
 }
+
+// jael.Document parse
